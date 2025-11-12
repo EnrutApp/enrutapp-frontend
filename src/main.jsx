d49a7bb4c2947a7dbf4c5728ca851d553a36d3df
haderrenteria13 <haderrenteria13az@gmail.com>
2025-11-11 20:44:24 -0500

    refactor: update main layout and application entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import './shared/styles/index.css'
-import Routes from './routes/Routes'
-import { AuthProvider } from './shared/context/AuthContext'
-import { ThemeProvider } from './shared/context/ThemeContext'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import './shared/styles/index.css';
+import Routes from './routes/Routes';
+import { AuthProvider } from './shared/context/AuthContext';
+import { ThemeProvider } from './shared/context/ThemeContext';
+import { MapProvider } from './shared/components/map';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <AuthProvider>
-        <RouterProvider router={Routes} />
+        <MapProvider>
+          <RouterProvider router={Routes} />
+        </MapProvider>
       </AuthProvider>
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
